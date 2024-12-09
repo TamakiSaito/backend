f8b5892287d67db58d4e44da5553d4b681496795
TamakiSaito <immacd1012@gmail.com>
2024-12-09 22:39:31 +0900

    feat: study backend

--- 2-node.js/app1/app.js
+++ 2-node.js/app1/app.js
@@ -6,8 +6,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/',(_req, res) => {
-  res.send('Hello World!');
+// app.get('/',(_req, res) => {
+//   res.send('Hello World!');
+// })
+
+app.use("/", (_req, res) => {
+  res.sendFile(__dirname + "/index.html")
 })
 
 app.listen(port, () => {
