3af106a8b47ff235e40be830e8977cdd21785a63
TamakiSaito <immacd1012@gmail.com>
2024-12-10 22:46:48 +0900

    feat: study backend

--- 2-node.js/2-api(back)/routes/users.js
+++ 2-node.js/2-api(back)/routes/users.js
@@ -13,7 +13,7 @@ const User = require("../models/User")
 
 // [GET] /users/:userId : IDに該当するユーザー照会
 router.get("/:userId?", (_req, res) => {
-  const useId = _req.params.userId
+  const userId = _req.params.userId
   if (userId) {
     for (let i = 0; i < User.length; i++) {
       if (userId === User[i].userId) {
@@ -27,5 +27,40 @@ router.get("/:userId?", (_req, res) => {
 })
 
   // [POST] /create : ユーザー登録
+  // req : userId, password, nameの本体をbodyという
+  router.post("/create", (req, res) => {
+    const userId = req.body.userId
+    const password = req.body.password
+    const name = req.body.name
+    const newUser = { userId, password, name }
+    User.push(newUser)
+    return res.send(User)
+  })
+
+  // [PUT] /update/:userId : IDに該当すつユーザー情報更新
+  router.put("/update/:userId", (req, res) => {
+    const userId = req.params.userId
+    const password = req.body.userId
+    const name = req.body.name
+    for (let i = 0; i < User.length; i++) {
+      if (userId === User[i].userId) {
+        User[i] = { userId, password, name }
+        return res.send(User[i])
+      }
+    }
+    return res.send(User[i])
+  })
+
+  // [DELETE] /delete/:userId : IDに該当するユーザー削除
+  router.delete("/delete/:userId", (req, res) => {
+    const userId = req.params.userId
+    for (let i = 0; i < User.length; i++) {
+      if (userId === User[i].userId) {
+        User.splice(i, i)
+        return res.send(User)
+      }
+    }
+    return res.status(404).send("User not found")
+  })
 
 module.exports = router
